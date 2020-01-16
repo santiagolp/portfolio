d506ee51514595b7f9fb4b96c3d6ae753bf100f0
Santiago Dellepiane <dellepiane.sj@gmail.com>
2020-01-16 16:24:09 -0300

    Updates

---
+++ precache-manifest.8cb0ff97e68bb755c2a2b4121012cae6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3172a20203e323cde0d9d6763f62962b",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "07e399c44b92032d57e0",
+    "url": "/portfolio/static/css/main.8ab6f918.chunk.css"
+  },
+  {
+    "revision": "ad8e8dccf92391a62d32",
+    "url": "/portfolio/static/js/2.50fdd86e.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/portfolio/static/js/2.50fdd86e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "07e399c44b92032d57e0",
+    "url": "/portfolio/static/js/main.3d5f141b.chunk.js"
+  },
+  {
+    "revision": "0d98471f6c3b69ca4251",
+    "url": "/portfolio/static/js/runtime-main.44087e9b.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/portfolio/static/media/logo.ee7cd8ed.svg"
+  }
+]);
